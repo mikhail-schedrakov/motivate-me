003316a771c607ab0d008cb2e529aa4602d9898e
Mikhail Schedrakov <mikhail.schedrakov@gmail.com>
2014-05-08 14:29:20 +0400

    Fix settings

--- project/project/settings.py
+++ project/project/settings.py
@@ -48,7 +48,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 )
 
 ROOT_URLCONF = 'project.urls'
